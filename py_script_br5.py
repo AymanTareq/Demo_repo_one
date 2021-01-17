6f59a2771c85008fb6c6310d169d08e564245361
AymanTareq <tareq.ctec02@gmail.com>
2021-01-17 22:36:23 +0600

    sorted the list after removing duplicate

--- py_script_br5.py
+++ py_script_br5.py
@@ -1,3 +1,14 @@
 print("I will merge branch4 and branch5 with main branch")
 print("As a result, features developed in branch4 and branch5 will be available in main branch")
 print("It's cool. :)")
+
+## Let's see another way to remvoe duplicate from a list
+
+lst1 = ['a','b','c','d','d','c','d','b']
+
+print("Before removing duplicate:",lst1)
+
+lst1 = list(set(lst1))
+lst1.sort()
+
+print("After removing duplicate:",lst1)
